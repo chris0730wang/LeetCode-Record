bc11a0dda63375308353fcac6fb6038f7621b3ec
chris0730wang <56030023+chris0730wang@users.noreply.github.com>
2022-07-27 21:22:40 +0800

    Time: 4 ms (49.94%), Space: 5.6 MB (34.88%) - LeetHub

---
+++ 7-reverse-integer/7-reverse-integer.c
@@ -0,0 +1,14 @@
+
+
+int reverse(int x){
+    int y=0;
+    while(x!=0){
+        if(y>INT_MAX/10||y<INT_MIN/10)return 0;
+        else if(y==INT_MAX/10||y==INT_MIN/10){
+            if(x%10>7||x%10<-8)return 0;
+        }
+        y=y*10+x%10;
+        x/=10;
+    }
+    return y;
+}
