b9a7dbe4e9f1032e770d821acc99e07499ade425
chris0730wang <56030023+chris0730wang@users.noreply.github.com>
2022-07-27 21:22:53 +0800

    Time: 26 ms (32.88%), Space: 6.4 MB (5.52%) - LeetHub

---
+++ 9-palindrome-number/9-palindrome-number.c
@@ -0,0 +1,18 @@
+
+
+bool isPalindrome(int x){
+    if(x<0)return false;
+    if(x==0)return true;
+    int temp, count=0, i;
+    char* arr = (char*)malloc(10);
+    while(x!=0){
+        temp=x%10;
+        arr[count]=temp;
+        count++;
+        x/=10;
+    }
+    for(i=0;i<count/2;i++){
+        if(arr[i]!=arr[count-1-i])return false;
+    }
+    return true;
+}
