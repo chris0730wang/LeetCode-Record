aadc8755b84b23f91fbceac69c36351da3500064
chris0730wang <56030023+chris0730wang@users.noreply.github.com>
2022-08-22 21:44:11 +0800

    Time: 146 ms (5.91%), Space: 6.6 MB (9.55%) - LeetHub

--- 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.c
+++ 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.c
@@ -5,6 +5,7 @@ int mostWordsFound(char ** sentences, int sentencesSize){
     for(int i=0; i<sentencesSize; i++){
         char *locate = sentences[i];
         while(*locate){
+            printf("%c", *locate);
             if(*locate == ' ') times++;
             *locate = *(sentences[i] + gap++);
         }
