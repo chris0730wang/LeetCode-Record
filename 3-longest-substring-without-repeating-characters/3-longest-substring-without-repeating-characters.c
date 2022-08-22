762742dc00d9278e46a41a7b40e3c942a3dfe650
chris0730wang <56030023+chris0730wang@users.noreply.github.com>
2022-08-22 14:09:44 +0800

    Time: 7 ms (72.14%), Space: 6.5 MB (6.61%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.c
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.c
@@ -1,19 +1,18 @@
 
 
 int lengthOfLongestSubstring(char * s){
-    int *ht = (int*)malloc(sizeof(int)*128);
+    int *ht = (int*)malloc(sizeof(int) * 128);
     for(int i=0; i<128; i++){
         ht[i] = -1;
     }
-    int len = strlen(s);
-    int substring_start = 0, max = 0;
+    int len = strlen(s), substring_start = 0, max = 0;
     for(int i=0; i<len; i++){
-        if(ht[s[i]] >= substring_start){
+        if(ht[s[i]] >= substring_start){ // has appeared
             substring_start = ht[s[i]] + 1;
             ht[s[i]] = i;
         }else{
             ht[s[i]] = i;
-            if(i-substring_start+1 > max) max = i-substring_start+1;
+            if(i - substring_start + 1 > max) max = i - substring_start + 1;
         }
     }
     return max;
