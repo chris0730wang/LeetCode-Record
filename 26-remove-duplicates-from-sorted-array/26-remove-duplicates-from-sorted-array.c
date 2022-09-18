4909d8c1be7adb5caf384b433675e92ec1611c0c
chris0730wang <56030023+chris0730wang@users.noreply.github.com>
2022-09-18 14:36:22 +0800

    Time: 28 ms (31.63%), Space: 7.8 MB (9.82%) - LeetHub

---
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.c
@@ -0,0 +1,15 @@
+
+int removeDuplicates(int* nums, int numsSize){
+    if (numsSize == 0) { return 0;}
+    int count = 1; 
+    int j = 0;      
+    
+    for (int i=0; i< numsSize-1; i++)  {
+        if (nums[i] != nums[i+1]) {
+            count++;
+            nums[i+1-j] = nums[i+1];
+        }
+        else j++;
+    } 
+    return count;
+}
