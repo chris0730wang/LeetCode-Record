88be95ec6461621ad4f399027770d90cf8f3c16b
chris0730wang <56030023+chris0730wang@users.noreply.github.com>
2024-03-19 22:10:28 +0800

    Time: 70 ms (41.22%) | Memory: 8.5 MB (6.76%) - LeetSync

--- 561-array-partition/array-partition.c
+++ 561-array-partition/array-partition.c
@@ -1,14 +1,18 @@
+int cmp(const void *a, const void *b){
+    return (*(int*)a - *(int*)b);
+}
 int arrayPairSum(int* nums, int numsSize) {
     int ret = 0;
-    for(int i = 1; i < numsSize; i++){
-        int tmp = nums[i];
-        int j = i - 1;
-        while(j >= 0 && tmp < nums[j]){
-            nums[j + 1] = nums[j];
-            j--;
-        }
-        nums[j + 1] = tmp;
-    }
+    qsort(nums, numsSize, sizeof(int), cmp);
+    // for(int i = 1; i < numsSize; i++){
+    //     int tmp = nums[i];
+    //     int j = i - 1;
+    //     while(j >= 0 && tmp < nums[j]){
+    //         nums[j + 1] = nums[j];
+    //         j--;
+    //     }
+    //     nums[j + 1] = tmp;
+    // }
     for(int i = 0; i < numsSize; i+=2){
         ret += nums[i];
     }
