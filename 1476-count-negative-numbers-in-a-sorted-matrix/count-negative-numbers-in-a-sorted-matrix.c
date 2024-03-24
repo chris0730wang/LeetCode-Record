2baf6fd21e83a50d83ce52f48015b45274107ef8
chris0730wang <56030023+chris0730wang@users.noreply.github.com>
2024-03-24 22:06:17 +0800

    Time: 11 ms (93.19%) | Memory: 6.5 MB (70.61%) - LeetSync

--- 1476-count-negative-numbers-in-a-sorted-matrix/count-negative-numbers-in-a-sorted-matrix.c
+++ 1476-count-negative-numbers-in-a-sorted-matrix/count-negative-numbers-in-a-sorted-matrix.c
@@ -1,7 +1,7 @@
 int countNegatives(int** grid, int gridSize, int* gridColSize) {
     int ret = 0;
     for(int i = 0; i < gridSize; i++){
-        if(grid[i][(*gridColSize) - 1] > 0) continue;
+        //if(grid[i][(*gridColSize) - 1] > 0) continue;
         for(int j = 0; j < (*gridColSize); j++){
             if(grid[i][j] < 0) {
                 ret += (*gridColSize) - j;
