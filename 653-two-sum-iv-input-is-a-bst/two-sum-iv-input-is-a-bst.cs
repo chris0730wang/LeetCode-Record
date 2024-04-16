f6bbe40458475d6a616ab5fb4336e40686872673
chris0730wang <56030023+chris0730wang@users.noreply.github.com>
2024-04-16 22:02:39 +0800

    Time: 119 ms (34.69%) | Memory: 53.4 MB (71.43%) - LeetSync

--- 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.cs
+++ 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.cs
@@ -12,26 +12,15 @@
  * }
  */
 public class Solution {
+    List<int> seenValues = new List<int>();
     public bool FindTarget(TreeNode root, int k) {
-        if(root.left == null && root.right == null) return false;
-
-        List<int> lstVal = new List<int>();
-        Queue<TreeNode> qNode = new Queue<TreeNode>();
-
-        qNode.Enqueue(root.left);
-        qNode.Enqueue(root.right);
-        lstVal.Add(root.val);
-
-        while(qNode.Count > 0){
-            TreeNode tmpNode = qNode.Dequeue();
-            if(tmpNode != null){
-                if(lstVal.Contains(k - tmpNode.val)) return true;
-                lstVal.Add(tmpNode.val);
-                qNode.Enqueue(tmpNode.left);
-                qNode.Enqueue(tmpNode.right);
-            }
+        if (root is null) {
+            return false;
         }
-
-        return false;
+        if (seenValues.Contains(k-root.val)){
+            return true;
+        }
+        seenValues.Add(root.val);
+        return FindTarget(root.left, k) || FindTarget(root.right, k);
     }
 }
