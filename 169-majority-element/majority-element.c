168d4d8fd8b8c1a65018e1352aba8cad225c2e4a
chris0730wang <56030023+chris0730wang@users.noreply.github.com>
2024-05-01 21:16:45 +0800

    Time: 41 ms (5.15%) | Memory: 8.3 MB (5.05%) - LeetSync

--- 169-majority-element/majority-element.c
+++ 169-majority-element/majority-element.c
@@ -1,7 +1,7 @@
 int cmp(const void *a, const void *b){
-	return(*(int*)a - *(int*)b);
+    return *(int*)a - *(int*)b;
 }
 int majorityElement(int* nums, int numsSize) {
     qsort(nums, numsSize, sizeof(int), cmp);
-	return nums[numsSize/2];
+    return nums[numsSize / 2];
 }
