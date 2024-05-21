9b146777b2dec589223917a12e0891248b8fcda5
chris0730wang <56030023+chris0730wang@users.noreply.github.com>
2024-05-21 22:00:32 +0800

    Time: 199 ms (5.03%) | Memory: 14.1 MB (21.69%) - LeetSync

---
+++ 287-find-the-duplicate-number/find-the-duplicate-number.c
@@ -0,0 +1,10 @@
+int cmp(const void *a, const void *b){
+    return *(int*)a - *(int*)b;
+}
+int findDuplicate(int* nums, int numsSize) {
+    qsort(nums, numsSize, sizeof(int), cmp);
+    for(int i = 1; i < numsSize; i++){
+        if(nums[i] == nums[i - 1]) return nums[i];
+    }
+    return 0;
+}
