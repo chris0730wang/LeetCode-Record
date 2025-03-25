389ef77ef1b4313eb5865d6c7c1cfc4a5a0eef4d
chris0730wang <56030023+chris0730wang@users.noreply.github.com>
2025-03-25 13:32:52 +0800

    Time: 16 ms (85.47%) | Memory: 67.3 MB (19.56%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public IList<IList<string>> GroupAnagrams(string[] strs) {
+        var res = new Dictionary<string, List<string>>();
+        foreach(string str in strs){
+            int[] count = new int[26];
+            foreach(char c in str){
+                count[c - 'a']++;
+            }
+            string key = string.Join(',', count);
+            if(!res.ContainsKey(key)){
+                res[key] = new List<string>();
+            }
+            res[key].Add(str);
+        }
+        return res.Values.ToList<IList<string>>();
+    }
+}
