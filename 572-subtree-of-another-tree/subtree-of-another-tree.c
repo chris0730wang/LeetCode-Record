dace6f4bb556ab219c2debf3c6837a61cd18b306
chris0730wang <56030023+chris0730wang@users.noreply.github.com>
2025-03-31 20:07:06 +0800

    Time: 0 ms (100.00%) | Memory: 14.4 MB (71.58%) - LeetSync

---
+++ 572-subtree-of-another-tree/subtree-of-another-tree.c
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+bool isSameNode(struct TreeNode* root1, struct TreeNode* root2){
+    if(!root1 && !root2) return true;
+    if(!root1 || !root2) return false;
+    if(root1->val != root2->val) return false;
+
+    return isSameNode(root1->left, root2->left) && isSameNode(root1->right, root2->right);
+}
+bool isSubtree(struct TreeNode* root, struct TreeNode* subRoot) {
+    if (!root) return 0; 
+    if (isSameNode(root, subRoot)) return 1; 
+
+    return isSubtree(root->left, subRoot) || isSubtree(root->right, subRoot);
+}
