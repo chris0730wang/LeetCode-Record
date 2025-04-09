e0a41e6334e8e3f909025adc184778bd1bcf6ce8
chris0730wang <56030023+chris0730wang@users.noreply.github.com>
2025-04-09 18:45:22 +0800

    Time: 0 ms (100.00%) | Memory: 8.6 MB (36.42%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.c
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.c
@@ -1,29 +1,31 @@
 char* reverseWords(char* s) {
     int len = strlen(s);
-    char* ret = (char*)malloc(sizeof(char) * (len + 1));
-    ret[len] = '\0';
-    int index = 0;
+    char* res = (char*)malloc(sizeof(char) * (len + 2));
+    char* temp = (char*)malloc(sizeof(char) * (len + 1));
+    int index = 0, idx = 0;
+    res[0] = '\0';
     for(int i = len - 1; i >= 0; i--){
-        if(s[i] == ' ') {
-            ret[index++] = ' ';
+        if(s[i] == ' ' && index != 0){
+            res[idx++] = ' ';
+            for(int j = index - 1; j >= 0; j--){
+                res[idx++] = temp[j];
+            }
+            temp[0] = '\0';
+            index = 0;
+        }
+        else if(s[i] == ' ') {
             continue;
         }
-        int right = i, left = i;
-        while(left > 0 && s[left - 1] != ' ') left--;
-        for(int j = left; j <= right; j++){
-            ret[index++] = s[j];
-        }
-        i = left;
-    }
-
-    index = 0;
-    for(int i = 0; ret[i]; i++){
-        if(ret[i] != ' ' || (i > 0 && ret[i - 1] != ' ')){
-            ret[index++] = ret[i];
+        else {
+            temp[index++] = s[i];
         }
     }
-    if(ret[index - 1] == ' ') ret[index - 1] = '\0';
-    else ret[index] = '\0';
-
-    return ret;
+    if(index != 0){
+        res[idx++] = ' ';
+        for(int j = index - 1; j >= 0; j--){
+            res[idx++] = temp[j];
+        }
+    }
+    res[idx] = '\0';
+    return res + 1;
 }
