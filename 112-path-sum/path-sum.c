3a92151966ba297a978b7a63fff90dd9e964b645
chris0730wang <56030023+chris0730wang@users.noreply.github.com>
2025-04-11 13:47:01 +0800

    Time: 0 ms (100.00%) | Memory: 11.5 MB (67.12%) - LeetSync

--- 112-path-sum/path-sum.c
+++ 112-path-sum/path-sum.c
@@ -6,11 +6,23 @@
  *     struct TreeNode *right;
  * };
  */
-bool hasPathSum(struct TreeNode* root, int targetSum) {
+
+bool check(struct TreeNode* root, int targetSum, int* sum){
     if(!root) return false;
-    targetSum -= root->val;
-    if(targetSum == 0 && !root->left && !root->right) return true;
-    else{
-        return hasPathSum(root->left, targetSum) || hasPathSum(root->right, targetSum);
+    
+    *sum += root->val;
+
+    if(root->left == NULL && root->right == NULL){
+        if(*sum == targetSum) return true;
     }
+
+    if(check(root->left, targetSum, sum) || check(root->right, targetSum, sum)) return true;
+
+    *sum -= root->val;
+    return false;
+} 
+
+bool hasPathSum(struct TreeNode* root, int targetSum) {
+    int sum = 0;
+    return check(root, targetSum, &sum);
 }
