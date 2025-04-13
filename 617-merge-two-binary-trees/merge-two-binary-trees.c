0943efaf332003eeb07e6bd943d81058ea923d65
chris0730wang <56030023+chris0730wang@users.noreply.github.com>
2025-04-13 23:36:54 +0800

    Time: 7 ms (9.64%) | Memory: 25.3 MB (7.23%) - LeetSync

--- 617-merge-two-binary-trees/merge-two-binary-trees.c
+++ 617-merge-two-binary-trees/merge-two-binary-trees.c
@@ -7,11 +7,14 @@
  * };
  */
 struct TreeNode* mergeTrees(struct TreeNode* root1, struct TreeNode* root2) {
-    if(!root1) return root2;
-    if(!root2) return root1; 
-    struct TreeNode* ret = root1;
-    ret->val += root2->val;
-    ret->left = mergeTrees(root1->left, root2->left);
-    ret->right = mergeTrees(root1->right, root2->right);
-    return ret;
+    struct TreeNode* newNode = NULL;
+    if(root1 || root2){
+        newNode = (struct TreeNode*)malloc(sizeof(struct TreeNode));
+        int val1 = root1 ? root1->val : 0;
+        int val2 = root2 ? root2->val : 0;
+        newNode->val = val1 + val2;
+        newNode->left = mergeTrees(root1 ? root1->left : NULL, root2 ? root2->left : NULL);
+        newNode->right = mergeTrees(root1 ? root1->right : NULL, root2 ? root2->right : NULL);
+    }
+    return newNode;
 }
