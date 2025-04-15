1aca4f3ad4e8b40af8882ae1b0e0bf02730f54f7
chris0730wang <56030023+chris0730wang@users.noreply.github.com>
2025-04-15 22:05:44 +0800

    Time: 64 ms (16.66%) | Memory: 19.6 MB (45.85%) - LeetSync

--- 283-move-zeroes/move-zeroes.c
+++ 283-move-zeroes/move-zeroes.c
@@ -1,16 +1,13 @@
 void moveZeroes(int* nums, int numsSize) {
     for(int i = 0; i < numsSize; i++){
         if(nums[i] == 0){
-            int idx = i + 1;
-            while(idx < numsSize){
-                if(nums[idx++] == 0) continue;
-                break;
-            }
-            if(idx - 1 == numsSize) break;
-            else{
-                nums[i] = nums[idx - 1];
-                nums[idx - 1] = 0;
-            }
+            int j = i + 1;
+            while(j < numsSize && nums[j] == 0) j++;
+            if(j == numsSize) return;
+            int tmp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = tmp;
         }
     }
+    return;
 }
