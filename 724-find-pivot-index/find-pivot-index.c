f05ce87ee2c63a14256f9883e1fbf4dd8ab111ac
chris0730wang <56030023+chris0730wang@users.noreply.github.com>
2025-04-16 21:14:34 +0800

    Time: 1 ms (30.38%) | Memory: 9.5 MB (93.33%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.c
+++ 724-find-pivot-index/find-pivot-index.c
@@ -1,11 +1,11 @@
 int pivotIndex(int* nums, int numsSize) {
-    int right = 0, left = 0, res = -1;
-    for(int i = 0; i < numsSize; i++) right += nums[i];
-    if(right - nums[0] == 0) return 0;
-    for(int i = 0; i < numsSize - 1; i++){
+    int total = 0, left = 0;
+    for(int i = 0; i < numsSize; i++) total += nums[i];
+
+    for(int i = 0; i < numsSize; i++) {
+        if(left == total - left - nums[i]) return i;
         left += nums[i];
-        right -= nums[i];
-        if(left == right - nums[i+1]) return i+1;
     }
-    return res;
+
+    return -1;
 }
