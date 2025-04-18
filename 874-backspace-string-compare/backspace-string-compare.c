151c71628ab39ef2655511a28f1009bcf03ad71e
chris0730wang <56030023+chris0730wang@users.noreply.github.com>
2025-04-18 15:26:26 +0800

    Time: 0 ms (100.00%) | Memory: 7.9 MB (64.18%) - LeetSync

--- 874-backspace-string-compare/backspace-string-compare.c
+++ 874-backspace-string-compare/backspace-string-compare.c
@@ -1,47 +1,18 @@
 bool backspaceCompare(char* s, char* t) {
-    int s_len = strlen(s);
-    int t_len = strlen(t);
-    char* s_ret = (char*)malloc(sizeof(char) * (s_len + 1));
-    char* t_ret = (char*)malloc(sizeof(char) * (t_len + 1));
-    s_ret[s_len] = '\0';
-    t_ret[t_len] = '\0';
-    int i = s_len - 1;
-    int idx = 0;
-    int del = 0;
-    while(i >= 0){
-        if(s[i] == '#'){
-            del++;
-        }
-        else if(del > 0){
-            del--;
-        }
-        else{
-            s_ret[idx++] = s[i];
-        }
-        i--;
+    char* sStack = (char*)malloc(sizeof(char) * 200);
+    char* tStack = (char*)malloc(sizeof(char) * 200);
+    int sTop = -1, tTop = -1;
+    for(int i = 0; i < strlen(s); i++){
+        if(s[i] == '#') sTop = sTop < 0 ? sTop : sTop - 1;
+        else sStack[++sTop] = s[i];
     }
-    s_ret[idx] = '\0';
-    i = t_len - 1;
-    idx = 0;
-    del = 0;
-    while(i >= 0){
-        if(t[i] == '#'){
-            del++;
-        }
-        else if(del > 0){
-            del--;
-        }
-        else{
-            t_ret[idx++] = t[i];
-        }
-        i--;
+    for(int i = 0; i < strlen(t); i++){
+        if(t[i] == '#') tTop = tTop < 0 ? tTop : tTop - 1;
+        else tStack[++tTop] = t[i];
     }
-    t_ret[idx] = '\0';
-    if(strlen(s_ret) != strlen(t_ret)) return false;
-    else
-        for(i = 0; i < strlen(s_ret); i++){
-            if(s_ret[i] != t_ret[i]) return false;
-        }
-    
+    if(sTop != tTop) return false;
+    for(int i = 0; i < sTop + 1; i++){
+        if(sStack[i] != tStack[i]) return false;
+    }
     return true;
 }
