c7de0edc3087fff83fce9aea2dab7f513c8e7e16
chris0730wang <56030023+chris0730wang@users.noreply.github.com>
2025-05-20 12:30:31 +0800

    Time: 3 ms (30.19%) | Memory: 30.3 MB (40.14%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.c
+++ 238-product-of-array-except-self/product-of-array-except-self.c
@@ -3,21 +3,17 @@
  */
 int* productExceptSelf(int* nums, int numsSize, int* returnSize) {
     *returnSize = numsSize;
-    int* ret = (int*)malloc(sizeof(int) * (*returnSize));
-    int value = 1, zerotimes = 0;
-    for(int i = 0; i < numsSize; i++){
-        if(nums[i] == 0) {
-            zerotimes++;
-            continue;
-        }
-        value *= nums[i];
+    int* res = (int*)malloc(sizeof(int) * (*returnSize));
+    int val = 1;
+    res[0] = 1;
+    for(int i = 1; i < numsSize; i++){
+        val *= nums[i - 1];
+        res[i] = val;
     }
-    if(zerotimes > 1){
-        return (int*)calloc(numsSize, sizeof(int));
+    val = 1;
+    for(int i = numsSize - 2; i >= 0; i--){
+        val *= nums[i + 1];
+        res[i] *= val;
     }
-    for(int i = 0; i < numsSize; i++){
-        if(zerotimes > 0 && nums[i] != 0) ret[i] = 0;
-        else ret[i] = nums[i] == 0 ? value : value / nums[i];
-    }
-    return  ret;
+    return res;
 }
