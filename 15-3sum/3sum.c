38e098b821dd64775c367e5d92c0b6c54dda3b5c
chris0730wang <56030023+chris0730wang@users.noreply.github.com>
2025-05-20 13:15:45 +0800

    Time: 31 ms (96.84%) | Memory: 39.4 MB (31.31%) - LeetSync

--- 15-3sum/3sum.c
+++ 15-3sum/3sum.c
@@ -7,15 +7,14 @@ int cmp(const void *a, const void *b){
     return (*(int*)a - *(int*)b);
 }
 int** threeSum(int* nums, int numsSize, int* returnSize, int** returnColumnSizes) {
-    
     int** res = (int**)malloc(sizeof(int*) * (numsSize * numsSize));
     *returnColumnSizes = (int*)malloc(sizeof(int*) * (numsSize * numsSize));
     *returnSize = 0;
+    int left, right, val;
     qsort(nums, numsSize, sizeof(int), cmp);
-    int left, right, i, target, val;
-    for(i = 0; i < numsSize - 2; i++){
+    for(int i = 0; i < numsSize - 2; i++){
         if(i > 0 && nums[i] == nums[i - 1]) continue;
-		if(nums[i] > 0) break;
+        if(nums[i] > 0) break;
         left = i + 1;
         right = numsSize - 1;
         while(left < right){
@@ -25,17 +24,17 @@ int** threeSum(int* nums, int numsSize, int* returnSize, int** returnColumnSizes
                 res[(*returnSize)][0] = nums[i];
                 res[(*returnSize)][1] = nums[left];
                 res[(*returnSize)][2] = nums[right];
-                (*returnColumnSizes)[*returnSize] = 3;
+                (*returnColumnSizes)[(*returnSize)] = 3;
                 (*returnSize)++;
                 while(left < right && nums[left] == nums[left + 1]) left++;
-				while(left < right && nums[right] == nums[right - 1]) right--;
-				left++;
-				right--;
+                while(left < right && nums[right] == nums[right - 1]) right--;
+                left++;
+                right--;
             }
             else if(val < 0){
                 left++;
             }
-            else {
+            else{
                 right--;
             }
         }
