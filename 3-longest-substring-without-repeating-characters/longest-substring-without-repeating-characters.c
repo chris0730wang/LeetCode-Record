c7e4af4ac6ca842755195380f411ea63d7317bfc
chris0730wang <56030023+chris0730wang@users.noreply.github.com>
2025-06-07 15:28:14 +0800

    Time: 15 ms (22.94%) | Memory: 9.4 MB (76.03%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.c
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.c
@@ -1,9 +1,9 @@
 int lengthOfLongestSubstring(char* s) {
-    int* ht = (int*)malloc(sizeof(int) * 128);
+    int *ht = (int*)malloc(sizeof(int) * 128);
     int start = 0, max = 0;
     for(int i = 0; i < 128; i++) ht[i] = -1;
     for(int i = 0; i < strlen(s); i++){
-        if(ht[s[i]] >= start){
+        if(ht[s[i]] >= start){ // start 是 substring 的起頭，> start 表示目前的 substring 出現過這個字元
             start = ht[s[i]] + 1;
             ht[s[i]] = i;
         }
