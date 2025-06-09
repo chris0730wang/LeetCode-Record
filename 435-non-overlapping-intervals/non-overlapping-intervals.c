2200b30b21a4f5a38685411d752cf5f64a1dc61d
chris0730wang <56030023+chris0730wang@users.noreply.github.com>
2025-06-09 21:44:56 +0800

    Time: 38 ms (42.86%) | Memory: 34.8 MB (83.12%) - LeetSync

--- 435-non-overlapping-intervals/non-overlapping-intervals.c
+++ 435-non-overlapping-intervals/non-overlapping-intervals.c
@@ -1,26 +1,18 @@
 int cmp(const void *a, const void *b){
     int* intA = *(int**)a;
     int* intB = *(int**)b;
-    return intA[0] - intB[0];
+    return intA[1] - intB[1];
 }
 int eraseOverlapIntervals(int** intervals, int intervalsSize, int* intervalsColSize) {
     qsort(intervals, intervalsSize, sizeof(int*), cmp);
-    int lastStart = intervals[0][0];
     int lastEnd = intervals[0][1];
     int res = 0;
     for(int i = 1; i < intervalsSize; i++){
         if(intervals[i][0] >= lastEnd){
-            lastStart = intervals[i][0];
             lastEnd = intervals[i][1];
         }
-        else if(intervals[i][0] == lastStart){
+        else{
             res++;
-            lastEnd = intervals[i][1] < lastEnd ? intervals[i][1] : lastEnd;
-        }
-        else if(intervals[i][0] > lastStart){
-            res++;
-            lastStart = intervals[i][1] < lastEnd ? intervals[i][0] : lastStart;
-            lastEnd = intervals[i][1] < lastEnd ? intervals[i][1] : lastEnd;
         }
     }
     return res;
